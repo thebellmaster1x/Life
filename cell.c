60d38bba8b911122253209d59d1d765ac8c30e53
Matt Koscica <mattcccp@gmail.com>
2016-05-04 13:47:11 -0500

    working neighbor function

--- cell.c
+++ cell.c
@@ -22,6 +22,64 @@ void initializeField( short int *cells ) {
 
 }
 
+int neighbors( short int *cells, int row, int col ) {
+
+    int neighbors ;
+
+    for( int rowMod = -1; rowMod <=1; rowMod++ ) {
+        for( int colMod = -1; colMod <=1; colMod++ ) {
+
+            // handle edges and corners
+            if( row + rowMod == -1 ) {
+                if ( col + colMod == -1 ) {
+                    // NW corner
+                    neighbors += cells[ (ROWMAX - 1) * COLMAX + (COLMAX - 1) ] ;
+                    continue ;
+                } else if ( col + colMod == COLMAX ) {
+                    // NE corner
+                    neighbors += cells[ (ROWMAX - 1) * COLMAX ] ;
+                    continue ;
+                } else {
+                    // top row
+                    neighbors += cells[ (ROWMAX - 1) * COLMAX + (col + colMod) ] ;
+                    continue ;
+                }
+            } else if ( row + rowMod == ROWMAX ) {
+                if ( col + colMod == -1 ) {
+                    // SW corner
+                    neighbors += cells[ (COLMAX - 1) ] ;
+                    continue ;
+                } else if ( col + colMod == COLMAX ) {
+                    // SE corner
+                    neighbors += cells[ 0 ] ;
+                    continue ;
+                } else {
+                    // bottom row
+                    neighbors += cells[ (col + colMod) ] ;
+                    continue ;
+                }
+            } else if ( col + colMod == -1 ) {
+                // left column
+                neighbors += cells[ (row + rowMod) * COLMAX + (COLMAX - 1 ) ] ;
+                continue;
+            } else if ( col + colMod == COLMAX ) {
+                // right column
+                neighbors += cells[ (row + rowMod) * COLMAX ] ;
+                continue;
+            } else {
+                // center field
+                neighbors += cells[ (row + rowMod) * COLMAX + (col + colMod) ] ;
+            }
+
+        }
+    }
+
+    neighbors -= cells[ row * COLMAX + col ] ;
+
+    return neighbors ; 
+
+}
+
 void printField( short int *cells ) {
 
     // clear field
@@ -35,7 +93,7 @@ void printField( short int *cells ) {
     for( int row = 0; row < ROWMAX; row++ ) {
         for( int col = 0; col < COLMAX; col++ ) {
             if( cells[ row * COLMAX + col ] == ON ) {
-                mvaddch( row, col, '#' | COLOR_PAIR(2) ) ;
+                mvaddch( row, col, neighbors( cells, row, col ) + '0' | COLOR_PAIR(2) ) ;
             }
         }
     }
--- cell.h
+++ cell.h
@@ -1,2 +1,3 @@
 void initializeField( short int *cells ) ;
+int neighbors( short int *cells, int row, int col ) ;
 void printField( short int *cells ) ;
